c416bfb7a9efd5dd8cc8cc45a48ca963fa1a1d85
Edward A Maxedon <edadma@vinctus.com>
2025-11-06 12:07:34 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'drive',
+  webDir: 'dist'
+};
+
+export default config;
